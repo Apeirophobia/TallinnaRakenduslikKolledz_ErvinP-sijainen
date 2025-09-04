5db8db7f1dc2e583a7bbd068e0d385745b3022fc
Apeirophobia <ervinpusijainen@gmail.com>
2025-09-04 13:01:00 +0300

    Inital commit
    Initial Commit acrophobia

---
+++ TallinnaRakenduslikKolledz/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace TallinnaRakenduslikKolledz.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set}
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+        /* Lisa kolm omadust */
+
+        public int? Smart { get; set; }
+        public int? Sleepy { get; set; }
+        public ICollection<Tracks>? Tracks { get; set; }
+    }
+}
