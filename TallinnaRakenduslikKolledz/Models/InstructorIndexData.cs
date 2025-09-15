7edb2a4dda41b25d19bcbb4098c1d14a51ad19f8
Apeirophobia <ervinpusijainen@gmail.com>
2025-09-15 14:29:15 +0300

    Added Instructors model, OfficeAssignments model, CourseAssignments model, Instructors controller, InstructorIndexModel

---
+++ TallinnaRakenduslikKolledz/Models/InstructorIndexData.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TallinnaRakenduslikKolledz.Models
+{
+    public class InstructorIndexData : Controller
+    {
+        public IEnumerable<Instructor> Instructions { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+        
+    }
+}
