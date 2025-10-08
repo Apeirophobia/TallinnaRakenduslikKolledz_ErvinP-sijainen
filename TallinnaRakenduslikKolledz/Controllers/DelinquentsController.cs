0a9b6e0f1da5e17e75a33013f9ec242ecd18f0e3
Apeirophobia <ervinpusijainen@gmail.com>
2025-10-08 10:45:43 +0300

    Delinquent Edit View, Get/Post

--- TallinnaRakenduslikKolledz/Controllers/DelinquentsController.cs
+++ TallinnaRakenduslikKolledz/Controllers/DelinquentsController.cs
@@ -79,5 +79,41 @@ namespace TallinnaRakenduslikKolledz.Controllers
             return View("Details", delinquent);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        { 
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var delinquent = await _context.Delinquents
+                .AsNoTracking().FirstOrDefaultAsync(d => d.DelinquentID == id);
+            
+            if (delinquent == null)
+            {
+                return NotFound();
+            }
+
+            PopulateViolationDropDownList();
+            return View("Edit", delinquent);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ConfirmEdit([Bind("DelinquentID, LastName, FirstName, Violation, DelinquentType, Description")] Delinquent delinquent)
+        {
+            if (delinquent.DelinquentID == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Delinquents.Update(delinquent);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View("Edit", delinquent);
+        }
     }
 }
